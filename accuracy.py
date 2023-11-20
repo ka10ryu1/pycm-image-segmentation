c457ae6747ea2b768f999d13bd2793fd0322b6f4
ka10ryu1 <lsyp.tuiovjo.1231@gmail.com>
2023-11-20 11:19:27 +0900

    :recycle: リファクタリング

--- accuracy.py
+++ accuracy.py
@@ -3,8 +3,19 @@
 # pylint: disable=invalid-name,no-member
 
 def dice(output, target, e=1e-9):
-    i_flat = output.contiguous().view(-1).clamp(min=0)
-    t_flat = target.contiguous().view(-1)
+    i_flat = (
+        output
+        .contiguous()
+        .view(-1)
+        .clamp(min=0)
+        .to('cpu')
+    )
+    t_flat = (
+        target
+        .contiguous()
+        .view(-1)
+        .to('cpu')
+    )
     _correct = 2 * (i_flat * t_flat).sum() + e
     _examples = (i_flat * i_flat).sum() + (t_flat * t_flat).sum() + e
-    return (_correct / _examples).to("cpu")
+    return _correct / _examples
